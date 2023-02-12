93b264332628c3b9806dfed0021177359f339ec1
LebitaA <adrian.lebita@ospri.co.nz>
2023-02-13 11:07:47 +1300

    Pipes and playing around with getters and setters.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-﻿import {Component, OnInit} from "@angular/core";
+﻿import {Component, OnDestroy, OnInit} from "@angular/core";
 import {IProduct} from "./Interfaces/IProduct";
 
 @Component({
@@ -7,18 +7,19 @@ import {IProduct} from "./Interfaces/IProduct";
   styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
+  private _productFilter: string = ""
+
   pageTitle: string = "Product List"
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  itemFilter: string = "";
 
   products: IProduct[] = [
     {
       "productId": 2,
       "productName": "Garden Cart",
-      "productCode": "GARDENCART01",
+      "productCode": "G-CART-01",
       "releaseDate": "01-01-2022",
       "description": "Choo choo",
       "price": 32.99,
@@ -29,7 +30,7 @@ export class ProductListComponent implements OnInit{
     {
       "productId": 5,
       "productName": "Hammer",
-      "productCode": "HAMMER01",
+      "productCode": "H-01",
       "releaseDate": "01-02-2022",
       "description": "Bang bang",
       "price": 12.99,
@@ -38,6 +39,24 @@ export class ProductListComponent implements OnInit{
       "stock": 5
     },
   ]
+
+  filteredProducts: IProduct[] = this.products;
+
+  filterProducts(): void{
+    this.filteredProducts = this.products
+      .filter(p => p.productName.toLowerCase()
+      .includes(this._productFilter.toLowerCase()));
+  }
+
+  get productFilter(): string{
+    return this._productFilter;
+  }
+
+  set productFilter(value: string){
+    this._productFilter = value;
+    this.filterProducts();
+  }
+
 
   toggleImage(): void {
     this.showImage = !this.showImage;
@@ -48,5 +67,12 @@ export class ProductListComponent implements OnInit{
     // The constructor is best used to initialise component's members/properties.
     // In terms of loading order:
     // Component is created, calls constructor -> component added to the DOM -> ngOnInit is then called.
+  }
+
+  ngOnDestroy(){
+    // Best place to unsubscribe from streams.
+    // Clean up logic.
+    // The component is still part of the DOM when this lifecycle hook is invoked. The component is removed from the
+    // DOM after completion of this method.
   }
 }
---
+++ src/app/shared/pipes/convert-to-spaces.pipe.ts
@@ -0,0 +1,13 @@
+﻿import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+  name: 'convertToSpaces' //This is the selector or name reference to call this pipe in HTML.
+})
+
+
+export class ConvertToSpacesPipe implements PipeTransform {
+  transform(word: string, value: string): any {
+
+    return word.replace(value, " ");
+  }
+}
